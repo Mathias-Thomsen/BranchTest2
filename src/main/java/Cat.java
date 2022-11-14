2f271365fe90e1ced962044bb5dffb1bdc652fbb
Mathias-Thomsen <math831t6@stud.kea.dk>
2022-11-14 13:11:56 +0100

    eat metode added

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -4,4 +4,9 @@ public class Cat extends Animal{
     public void makeSound() {
         System.out.println("miaw");
     }
+
+    @Override
+    public boolean eat(String foodType) {
+        return true;
+    }
 }
